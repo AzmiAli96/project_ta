e359bad26e5612b0528652e98d8fa86c8f1d96d8
GHfarhanarya <127170984+GHfarhanarya@users.noreply.github.com>
2024-07-08 01:46:46 +0700

    seeder data mahasiswa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ProgramSeeder::class,
+            MahasiswaSeeder::class
         ]);
 
         // User::factory(10)->create();
---
+++ database/seeders/Mahasiswaseeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MahasiswaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \App\Models\User::create([
+            'name'=> 'Muhammad Farhan Arya',
+            'email'=> 'mfarhanarya9@gmail.com',
+            'password'=> bcrypt('123123123'),
+            'level'=> 'Mahasiswa',
+        ]);
+
+        \App\Models\Mahasiswa::create([
+            'nobp'=> '2211083036',
+            'namalengkap'=> 'Muhammad Farhan Arya',
+            'user_id'=> '7',
+            'jurusan_id'=> '1',
+            'prodi_id'=> '1',
+        ]);
+
+        \App\Models\User::create([
+            'name'=> 'Hans Demberger S',
+            'email'=> 'hansd@gmail.com',
+            'password'=> bcrypt('123123123'),
+            'level'=> 'Mahasiswa',
+        ]);
+
+        \App\Models\Mahasiswa::create([
+            'nobp'=> '2211081012',
+            'namalengkap'=> 'Hans Demberger S',
+            'user_id'=> '8',
+            'jurusan_id'=> '1',
+            'prodi_id'=> '1',
+        ]);
+
+        \App\Models\User::create([
+            'name'=> 'Muhammad Rizky',
+            'email'=> 'siki@gmail.com',
+            'password'=> bcrypt('123123123'),
+            'level'=> 'Mahasiswa',
+        ]);
+
+        \App\Models\Mahasiswa::create([
+            'nobp'=> '2211082222',
+            'namalengkap'=> 'Muhammad Rizky',
+            'user_id'=> '9',
+            'jurusan_id'=> '1',
+            'prodi_id'=> '1',
+        ]);
+
+       
+
+    }
+}
